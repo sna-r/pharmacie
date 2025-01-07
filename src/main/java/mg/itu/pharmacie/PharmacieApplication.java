a0d4b4798902fbade04b6b5d5b4ded4d22bd31e4
SNyAvo <sitrakanyavorabenaivo@gmail.com>
2025-01-07 13:06:14 +0300

    first

---
+++ src/main/java/mg/itu/pharmacie/PharmacieApplication.java
@@ -0,0 +1,13 @@
+package mg.itu.pharmacie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacieApplication.class, args);
+	}
+
+}
